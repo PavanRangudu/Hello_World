473e6f8ec5ac849c22b537820f79c30b98d69e2b
Nitin Jain <nitinjain.2408@gmail.com>
2018-05-12 17:06:57 +0530

    Add files via upload
    hello_world

---
+++ hello.java
@@ -0,0 +1,10 @@
+package hello_world;
+
+public class hello {
+
+	public static void main (String s[]) {
+		System.out.println("Hello World!");
+		System.out.print("dsjfksd");
+		System.out.print("\n",s[1]);
+	}
+}
